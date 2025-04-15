75f91390683f15bd582e0bca057c6e1cdc6e6a96
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-04-15 16:41:59 +0530

    Merge branch 'main' of https://github.com/AmaanSayyad/Starknet-Arcade

---
+++ client/app/plinko/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlinkoGame from '../components/plinko/PlinkoGame'
+
+const Plinko = () => {
+  return (
+    <>
+        <PlinkoGame/>
+    </>
+  )
+}
+
+export default Plinko
