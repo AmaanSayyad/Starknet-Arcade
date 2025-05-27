2e25506463be7d3d6a8e95f061733f34e5d1d3b1
Amaan <amaansayyad@yahoo.com>
2025-05-27 16:24:55 +0530

    minor changes

--- client/app/community/components/FAQSection.tsx
+++ client/app/community/components/FAQSection.tsx
@@ -141,7 +141,17 @@ export default function FAQSection() {
             Ask on Discord
           </motion.a>
           
-        
+          <motion.a
+            href="/support"
+            className="flex items-center px-6 py-3 bg-gray-700 hover:bg-gray-600 text-white rounded-lg transition-colors"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+            </svg>
+            Contact Support
+          </motion.a>
           
           <motion.a
             href="/docs"
